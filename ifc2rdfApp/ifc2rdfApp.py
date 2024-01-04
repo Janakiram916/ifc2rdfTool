ec6c6f376a841b352be6625647dbbb3a7ad1ac90
Janakiram Karlapudi <janakiram.karlapudi@gropyus.com>
2024-01-04 18:31:54 +0100

    Extended the app lib

--- ifc2rdfApp/ifc2rdfApp.py
+++ ifc2rdfApp/ifc2rdfApp.py
@@ -1,7 +1,5 @@
-import logging
 import os.path
 import traceback
-from pathlib import Path
 
 import flet as ft
 from icecream import ic
@@ -12,10 +10,17 @@ from ifc2rdftool.ifc2rdf_tool import create_rdf_graph_from_ifc
 def main(page: ft.Page):
     def generate_rdf_file(e):
         ifc_file = ifc_file_path.value
-        ic(ifc_file)
-        ic(rdf_dir_path.value)
-        if os.path.isfile(ifc_file) and os.path.isdir(rdf_dir_path.value):
-            rdf_path = os.path.join(rdf_dir_path.value, f"{str(ifc_file_name.value).split('.')[0]}.ttl")
+        rdf_dir = rdf_dir_path.value
+        if not ifc_file:
+            user_info.value = "Error: Please select IFC file for the conversion"
+            user_info.update()
+            breakpoint()
+        if not rdf_dir:
+            user_info.value = "Error: Please select RDF repo for storing the generated graph"
+            user_info.update()
+            breakpoint()
+        if os.path.isfile(ifc_file) and os.path.isdir(rdf_dir):
+            rdf_path = os.path.join(rdf_dir, f"{str(ifc_file_name.value).split('.')[0]}.ttl")
             if os.path.exists(rdf_path):
                 ic("Removing existed RDF Graph")
                 os.remove(rdf_path)
@@ -26,11 +31,19 @@ def main(page: ft.Page):
                 graph.serialize(destination=rdf_path, format='ttl')
             except Exception as e:
                 ic(f"An unexpected error occurred: {e}")
-                traceback.print_exc()
+                traceback_str = traceback.format_exc()
+                user_info.value = traceback_str
+                user_info.update()
             else:
-                ic("RDF Graph generation is successful")
+                user_info.value = "RDF Graph generation is successful"
+                user_info.update()
         else:
-            ic(f"Either {rdf_dir_path.value} is not a valid directory or {ifc_file} is not a valid file")
+            ic(f"Either {rdf_dir} is not a valid directory or {ifc_file} is not a valid file")
+            user_info.value = f"Either {rdf_dir} is not a valid directory or {ifc_file} is not a valid file"
+            user_info.update()
+
+    user_info = ft.Text(color=ft.colors.AMBER)
+    generate_rdf_button = ft.ElevatedButton("Get_RDF", on_click=generate_rdf_file, icon=ft.icons.BUILD)
 
     def pick_ifc_files(e: ft.FilePickerResultEvent):
         if e.files:
@@ -42,8 +55,8 @@ def main(page: ft.Page):
             "File is not loaded successfully"
 
     pick_ifc_dialog = ft.FilePicker(on_result=pick_ifc_files)
-    ifc_file_name = ft.Text()
-    ifc_file_path = ft.Text()
+    ifc_file_name = ft.Text(color=ft.colors.AMBER_500)
+    ifc_file_path = ft.Text(italic=True, width=800, selectable=True)
     page.overlay.append(pick_ifc_dialog)
 
     def pick_rdf_file_dir(e: ft.FilePickerResultEvent):
@@ -54,24 +67,24 @@ def main(page: ft.Page):
             "Folder is not loaded successfully"
 
     pick_rdf_file_dir_dialog = ft.FilePicker(on_result=pick_rdf_file_dir)
-    rdf_dir_path = ft.Text()
+    rdf_dir_path = ft.Text(italic=True, width=800, selectable=True)
     page.overlay.append(pick_rdf_file_dir_dialog)
 
-    page.title = "IFC2RDF_TOOL"
-    page_heading = ft.Text(value="IFC2RDF_TOOL", color="blue", scale=5)
-    generate_rdf_button = ft.ElevatedButton("Get_RDF", on_click=generate_rdf_file, icon=ft.icons.BUILD)
+    page.title = "IFC2RDF (©Karlapudi)"
+    page_heading = ft.Text(value="IFC2RDF", color=ft.colors.BLACK, scale=3)
     page_width = 800
+    page.scroll = "auto"
 
     page.add(ft.Container(
         content=ft.Row(
             controls=[page_heading],
             alignment="center"
         ),
-        padding=20,
+        padding=10,
         alignment=ft.alignment.center,
-        bgcolor=ft.colors.GREEN_200,
+        bgcolor=ft.colors.PINK_400,
         width=page_width,
-        height=100,
+        height=80,
         border_radius=10,
     ),
         ft.Container(
@@ -83,7 +96,6 @@ def main(page: ft.Page):
                             icon=ft.icons.UPLOAD_FILE,
                             on_click=lambda _: pick_ifc_dialog.pick_files(allowed_extensions=["ifc"]),
                             autofocus=True,
-
                         ),
                         ]
                     ),
@@ -105,49 +117,36 @@ def main(page: ft.Page):
                         rdf_dir_path
                     ]),
                     ft.Row([generate_rdf_button]),
+
                 ],
                 alignment='center'
             ),
-            padding=20,
+            padding=10,
             alignment=ft.alignment.center,
             bgcolor=ft.colors.BLUE,
             width=page_width,
-            height=400,
+            height=350,
             border_radius=10,
-        )
+        ),
+        ft.Container(
+            content=ft.ListView(
+                controls=[
+                    user_info,
+                ],
+                expand=True,
+                spacing=10,
+                padding=10,
+                auto_scroll=True,
+            ),
+            padding=10,
+            alignment=ft.alignment.center,
+            bgcolor=ft.colors.WHITE,
+            width=page_width,
+            height=150,
+            border_radius=10,
+        ),
 
     )
-
-
-ft.app(target=main)
-
-# import flet as ft
-
-
-# def main(page: ft.Page):
-#     def pick_files_result(e: ft.FilePickerResultEvent):
-#         selected_files.value = (
-#             ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
-#         )
-#         selected_files.update()
-#
-#     pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-#     selected_files = ft.Text()
-#
-#     page.overlay.append(pick_files_dialog)
-#
-#     page.add(
-#         ft.Row(
-#             [
-#                 ft.ElevatedButton(
-#                     "Pick files",
-#                     icon=ft.icons.UPLOAD_FILE,
-#                     on_click=lambda _: pick_files_dialog.pick_files(),
-#                 ),
-#                 selected_files,
-#             ]
-#         )
-#     )
 
 
 ft.app(target=main)
